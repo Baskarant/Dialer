9be2ddc4cb2f4abe290b8013c6ee700be50df694
Baskarant <baskar159@gmail.com>
2017-07-29 22:32:15 +0530

    Add Button

---
+++ Dialer/Dialer/DialerVC.swift
@@ -0,0 +1,17 @@
+//
+//  DialerVC.swift
+//  Dialer
+//
+//  Created by Baskaran T on 29/07/17.
+//  Copyright © 2017 Baskaran T. All rights reserved.
+//
+
+import UIKit
+
+class DialerVC: UIViewController {
+    
+    @IBAction func dialDigit(_ sender: UIButton) {
+        print("7")
+    }
+    
+}
